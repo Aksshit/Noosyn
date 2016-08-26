de9d7bf0b74c6d18cf8ad9464ab1596b72217f35
Aksshit <aksshit.dewan@noosyntech.in>
2016-08-26 14:11:08 +0530

    Add files via upload

---
+++ odd even.py
@@ -0,0 +1,17 @@
+import math
+number = input("please input any no.")
+
+num = input("Enter a number: ")
+mod = num % 2
+if mod > 0:
+    print("You picked an odd number.")
+else:
+    print("You picked an even number.")
+
+
+if number % 2 == 0:
+    print("you have entered an even no")
+
+else:
+    print("you have entered and odd no")
+
